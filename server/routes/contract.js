46319941643c38ce85054ff5aec7ade63a42874a
varun1729 <varun.narayanan1729@gmail.com>
2018-03-31 15:12:39 -0700

    updates

--- server/routes/contract.js
+++ server/routes/contract.js
@@ -7,15 +7,12 @@ let mongoose = require('mongoose');
 //Models
 let Contract = require('../models/contract');
 
-const contractError = {
-    "message": "Error Contract not found."
-}
-
-
 router.get("/contracts", (req, res) => {
     Contract.find({ active: true }).lean().exec((error, contracts) => {
         if (error) {
-            res.json(contractError);
+            res.json({
+                "message": "Error Contract not found."
+            });
         } else {
             res.json(contracts);
         }
@@ -26,49 +23,55 @@ router.get("/contract/:address", (req, res) => {
     if (req.params.address != null) {
         Contract.findById(req.params.address, (error, contract) => {
             if (error || contract == null) {
-                res.send(contractError);
+                res.send({
+                    "message": "Error Contract not found."
+                });
             } else {
                 res.json(contract);
             }
         });
     } else {
-        res.send(contractError);
+        res.send({
+            "message": "Error Contract not found."
+        });
     }
 });
 
 router.post("/contract", (req, res) => {
     let address = req.body.address;
-    if(address){
-	    Contract.find({
-	        address: address
-	    }, (error, foundContracts) => {
-	        if (error) {
-	            res.json(contractCreationFailed);
-	        } else {
-	            if (foundContracts.length == 0) {
-	                Contract.create({
-	                    address: address
-	                }, err => {
-	                    if (err) {
-	                        res.json({
-	                            "message": "Error contract creation failed."
-	                        });
-	                    } else {
-	                        res.json({ "message": "Contract created succesfully." });
-	                    }
-	                });
-	            } else {
-	                res.json({ "message": "Contract with this address already exists." });
-	            }
-	        }
-	    });
-	} else {
-		res.json({"message": "Error address is undefined."})
-	}
+    if (address) {
+        Contract.find({
+            address: address
+        }, (error, foundContracts) => {
+            if (error) {
+                res.json({
+                    "message": "Error contract creation failed."
+                });
+            } else {
+                if (foundContracts.length == 0) {
+                    Contract.create({
+                        address: address
+                    }, err => {
+                        if (err) {
+                            res.json({
+                                "message": "Error contract creation failed."
+                            });
+                        } else {
+                            res.json({ "message": "Contract created succesfully." });
+                        }
+                    });
+                } else {
+                    res.json({ "message": "Contract with this address already exists." });
+                }
+            }
+        });
+    } else {
+        res.json({ "message": "Error address is undefined." })
+    }
 });
 
 router.put("/contract/:address", (req, res) => {
-    Contract.findOne({ address: req.params.address }, async(error, contract) => {
+    Contract.findOne({ address: req.params.address }, (error, contract) => {
         if (error) {
             console.log(error);
             res.json({
@@ -79,7 +82,15 @@ router.put("/contract/:address", (req, res) => {
                 res.json(contractError);
             } else {
                 contract.active = false;
-                res.json({ "message": "Contract updated succesfully." });
+                contract.save(error => {
+                    if (error) {
+                        res.json({
+                            "message": "Error contract updation failed."
+                        });
+                    } else {
+                        res.json({ "message": "Contract updated succesfully." });
+                    }
+                })
             }
         }
     });
