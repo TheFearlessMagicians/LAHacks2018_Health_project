7e42a06a4861392c683ee4fa80cb2ac190442943
willyspinner <wilsonjusuf1998@gmail.com>
2018-03-31 13:43:19 -0700

    mgconv ok

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -8,7 +8,7 @@ class LandingPage extends React.Component {
         name:''
     }
     render() {
-        
+
         return (
     	<div className = "container__landing">
         <Input placeholder="What's your name?" onChange={(e)=>{
@@ -20,11 +20,7 @@ class LandingPage extends React.Component {
                     alert('put your name please!');
                 else{
                     this.props.dispatch(addBetter({name:this.state.name}))
-<<<<<<< HEAD
         			this.props.history.push('/marketDashboard');
-=======
-        			this.props.history.push('/MarketDashBoardPage');
->>>>>>> bb43692b28f46c738724e8efa36ba701f156833c
                 }
     		}}>Better</Button>
 
