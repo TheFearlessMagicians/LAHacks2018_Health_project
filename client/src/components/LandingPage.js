43eb1cfefd3d7f8de18854efe03a3bb2353bc056
Leoputera2407 <cookieavarice@gmail.com>
2018-04-01 04:22:48 -0700

    styling

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -1,45 +1,67 @@
-import React from 'react';
-import {Button, Icon, Input} from  'antd';
-import {connect} from 'react-redux';
-import {addUser,addBetter,removePlayer} from '../actions/player';
+import React from "react";
+import { Button, Icon, Input } from "antd";
+import { connect } from "react-redux";
+import { addUser, addBetter, removePlayer } from "../actions/player";
 
 class LandingPage extends React.Component {
-    componentDidMount(){
+    componentDidMount() {
         this.props.dispatch(removePlayer());
     }
     state = {
-        name:''
-    }
+        name: ""
+    };
     render() {
-
         return (
-    	<div className = "container__landing ">
-        <h2> Start something, and do it. </h2>
-        <Input placeholder="What's your name?" onChange={(e)=>{
-            this.setState({name: e.target.value});
-        }} />
-    		<Icon type="line-chart" style={{ fontSize: 16, color: white }} />
-    		<Button onClick= {(e) => {
-                if (this.state.name=== '')
-                    alert('put your name please!');
-                else{
-                    this.props.dispatch(addBetter({name:this.state.name}))
-        			this.props.history.push('/marketDashboard');
-                }
-    		}}>Better</Button>
+            <div className="container__landing ">
+                <div className ="container__landing__bar">
+                    <h2> Start something, and do it. </h2>
+                    <Input
+                        className="container__landing__bar__input"
+                        placeholder="What's your name?"
+                        onChange={e => {
+                            this.setState({ name: e.target.value });
+                        }}
+                    />
+                    <Icon
+                        type="line-chart"
+                        style={{ fontSize: 16, color: "black" }}
+                    />
+                    <Button
+                        onClick={e => {
+                            if (this.state.name === "")
+                                alert("put your name please!");
+                            else {
+                                this.props.dispatch(
+                                    addBetter({ name: this.state.name })
+                                );
+                                this.props.history.push("/marketDashboard");
+                            }
+                        }}
+                    >
+                        Better
+                    </Button>
 
-    		<Icon type="user-add" style={{ fontSize: 16, color: white }} />
-    		<Button onClick= {(e) => {
-                if (this.state.name=== '')
-                    alert('put your name please!');
-                else{
-                    this.props.dispatch(addUser({name:this.state.name}))
-                    this.props.history.push('/userDashboard');
-                }
-
-    		}}>User</Button>
-    	</div>
+                    <Icon
+                        type="user-add"
+                        style={{ fontSize: 16, color: "black" }}
+                    />
+                    <Button
+                        onClick={e => {
+                            if (this.state.name === "")
+                                alert("put your name please!");
+                            else {
+                                this.props.dispatch(
+                                    addUser({ name: this.state.name })
+                                );
+                                this.props.history.push("/userDashboard");
+                            }
+                        }}
+                    >
+                        User
+                    </Button>
+                </div>
+            </div>
         );
     }
 }
-export default connect()( LandingPage);
+export default connect()(LandingPage);
