2f0c3f4bb87b6607c7cfaa87a411bc2c50c9726f
willyspinner <wilsonjusuf1998@gmail.com>
2018-04-01 04:20:22 -0700

    typewriter

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Button, Icon, Input} from  'antd';
 import {connect} from 'react-redux';
 import {addUser,addBetter,removePlayer} from '../actions/player';
-
+import TypeWriter from 'react-typewriter';
 class LandingPage extends React.Component {
     componentDidMount(){
         this.props.dispatch(removePlayer());
@@ -14,7 +14,11 @@ class LandingPage extends React.Component {
 
         return (
     	<div className = "container__landing ">
-        <h2> Start something, and do it. </h2>
+
+           <TypeWriter
+                typing={1}
+                fixed={true}>
+           <h1>Start something, and do it.</h1></TypeWriter>
         <Input placeholder="What's your name?" onChange={(e)=>{
             this.setState({name: e.target.value});
         }} />
@@ -38,6 +42,35 @@ class LandingPage extends React.Component {
                 }
 
     		}}>User</Button>
+
+
+            <TypeWriter
+                 typing={1}
+                 fixed={true}
+                 initDelay={500}
+                 >
+                <blockquote>
+                    I want to lose 5 kg of fat by running in 5 months.
+                    </blockquote>
+                 </TypeWriter>
+                 <TypeWriter
+                      typing={1}
+                      fixed={true}
+                      initDelay={1000}
+                     >
+                         <blockquote>
+                If I'm not ready for the Boston half-marathon in 6 months time I'm gonna pay $1000 for it. Fuck it.
+                    </blockquote>
+                 </TypeWriter>
+                 <TypeWriter
+                      typing={1}
+                      fixed={true}
+                      initDelay={2000}
+                     >
+                         <blockquote>
+                    I want to run around Venice Beach 3 times a week.
+                        </blockquote>
+                 </TypeWriter>
     	</div>
         );
     }
