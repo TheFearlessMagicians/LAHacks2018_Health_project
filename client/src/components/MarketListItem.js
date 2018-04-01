b8f9c83ac65a64b646a621f27e3e11cd0defa197
willyspinner <wilsonjusuf1998@gmail.com>
2018-04-01 03:11:38 -0700

    market list done

--- client/src/components/MarketListItem.js
+++ client/src/components/MarketListItem.js
@@ -5,26 +5,32 @@ import moment from 'moment';
 import 'antd/dist/antd.css';
 
 
-class MarketItemList extends React.Component{
+class MarketListItem extends React.Component{
 
-  state = { visible: false }
+
+  state = {
+      visible: false,
+  };
 
   showModal = () => {
     this.setState({
       visible: true,
+      bet: 0
     });
   }
 
   openNotificationWithIcon = (type) => {
   notification[type]({
     message: 'Bet Placed',
-    description: 'You have placed a bet of ',
+    description: `You have placed a bet of ${this.state.bet}`,
     duration: 3.0
   	});
   };
 
   handleOk = (e) => {
     this.openNotificationWithIcon('info');
+    //TODO: set state with bet.
+    this.props.onPlacedBet(this.props.challengeId,this.state.bet);
     this.setState({
       visible: false,
     });
@@ -43,14 +49,24 @@ class MarketItemList extends React.Component{
     return (
 	<List.Item
     	actions= {[
-        <p> End Date: :{moment(this.props.endDate).format('MM DD YYYY')}</p>,
-        <Button type="primary" onClick={this.showModal}>Click for More Info</Button>
+        <p key="1"> End Date: :{moment(this.props.endDate).format('MMM DD YYYY')}</p>,
+        <Button key="2" type="primary" onClick={this.showModal}>Click for More Info</Button>
     	]}
     	>
       <List.Item.Meta
         avatar={<Icon type="line-chart" />}
         title={<p onClick={this.showModal}>{this.props.goal}</p>}
-        description= {<p> User Bets: {this.props.userBet} </p>}
+        description= {[
+            <p key="0">{this.props.description}</p>,
+            <p key = "1"> User Bets: {this.props.userBet} </p>,
+            <p key = "2"> total pot: {this.props.bets.length !== 0?
+            this.props.bets.reduce((accumulator, bet)=> {
+                return {money: Number(accumulator.money) + Number(bet.money)};
+            }).money:
+            0
+            } </p>,
+            <p key = "3">number of bets: {this.props.bets.length}</p>
+        ]}
      />
      <Modal
           visible={this.state.visible}
@@ -59,16 +75,30 @@ class MarketItemList extends React.Component{
           onOk={this.handleOk}
           onCancel={this.handleCancel}
         >
+
           <p>Description: {this.props.description}</p>
           <p>User's Bet: {this.props.userBet}</p>
+          <p> total pot: {this.props.bets.length !== 0?
+          this.props.bets.reduce((accumulator, bet)=> {
+              return {money: Number(accumulator.money) + Number(bet.money)};
+          }).money:
+          0
+          } </p>
+          <p>number of bets: {this.props.bets.length}</p>
           <h3>{moment(this.props.endDate).from(moment(this.props.startDate))}</h3>
           <div>
 	          <Timeline>
-			    <Timeline.Item>User started the challenge on {moment(this.props.startDate).format("MM DD YYYY")}</Timeline.Item>
-			    <Timeline.Item dot={<Icon type="clock-circle-o" style={{ fontSize: '16px' }} />} color="red">Challenge ends on {moment(this.props.endDate).format("MM DD YYYY")}</Timeline.Item>
+			    <Timeline.Item>User started the challenge on {moment(this.props.startDate).format("MMM DD YYYY")}</Timeline.Item>
+			    <Timeline.Item dot={<Icon type="clock-circle-o" style={{ fontSize: '16px' }} />} color="red">Challenge ends on {moment(this.props.endDate).format("MMM DD YYYY")}</Timeline.Item>
 			  </Timeline>
 		  </div>
-		  <Input type = "number" placeholder="Insert Your Bet" />
+		  <Input
+            type = "number"
+            placeholder="Insert Your Bet"
+            value={this.state.bet}
+            onChange={(e)=>{
+                this.setState({bet:e.target.value})
+            }}/>
 
         </Modal>
     </List.Item>
@@ -76,7 +106,7 @@ class MarketItemList extends React.Component{
   }
 }
 
-export default MarketItemList;
+export default MarketListItem;
 
 // <div className = "market-item">
 //       	<p className = "market-item__text">{this.props.description}</p>
